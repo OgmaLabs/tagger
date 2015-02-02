32367c34954f9dd87c7a0684d4c20712a5db5de6
Ramon Rodrigues <cerberus.ramon@gmail.com>
2015-02-02 18:13:39 +0000

    Created duplication and tag list checking methods

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
-  //  allowDuplicates: true,
-  //  data: null,
-  //  delimiters: [9, 13, 44],
-  //  minTags: -1,
-  //  maxTags: -1,
-  //  onlyTagList: false,
-  //  tagCloseIcon: 'X',
-  //  tagClass: 'label',
-  //  tagsContainer: null,
-  //  tagList: null,
-  //  validator: null
-  $('#tagger-container').tagger({
-    allowDuplicates: false,
-    data: ['Apple', 'Oranges', 'Peanuts']
-    onlyTagList: true
-  })
+  // allowDuplicates: true,
+  // delimiters: [9, 13, 44],
+  // minTags: -1,
+  // maxTags: -1,
+  // onlyTagList: false,
+  // inputId: null,
+  // tagCloseIcon: 'X',
+  // tagClass: 'label',
+  // tagsContainerId: 'tag-content',
+  // tagList: null,
+  // tagListContainerId: null,
+  // validator: null,
+
+  $(document).foundation();
+  // Starting tag lib
+  tagger.inputId = 'tagger-container';
+  tagger.tagList = ['Apple', 'Oranges', 'Peanuts'];
+  tagger.tagListContainerId = 'tag-drop';
+  tagger.labelClass = 'secondary'
+  tagger.init();
 })
--- js/tagger.js
+++ js/tagger.js
@@ -1,114 +1,102 @@
-(function($){
-  "use strict";
+tagger = {
+  allowDuplicates: true,
+  delimiters: [9, 13, 44],
+  minTags: -1,
+  maxTags: -1,
+  onlyTagList: false,
+  inputId: null,
+  labelClass: null,
+  tagCloseIcon: 'X',
+  tagClass: 'label',
+  tagsContainerId: 'tag-content',
+  tagList: null,
+  tagListContainerId: null,
+  validator: false,
+  addTag: function(tag){
+    console.log(tag)
+    var self = $(this)[0];
+    var html = "<span class='label "+self.labelClass+"'>"+tag+" <a href='#'>"+self.tagCloseIcon+"</a></span>";
 
-  var defaults =  {
-    allowDuplicates: true,
-    data: null,
-    delimiters: [9, 13, 44],
-    minTags: -1,
-    maxTags: -1,
-    onlyTagList: false,
-    tagCloseIcon: 'X',
-    tagClass: 'label',
-    tagsContainer: null,
-    tagList: null,
-    validator: null
+    if(!self.allowDuplicates && self.onlyTagList && self.noDuplicate(tag) && self.tagListChecker(tag)){
+      $('#tag-content').append(html)
+    }else if(!self.allowDuplicates && self.onlyTagList && self.noDuplicate(tag)){
+      $('#tag-content').append(html)
+    }else if(self.allowDuplicates && !self.onlyTagList && self.tagListChecker(tag)){
+      $('#tag-content').append(html)
+    }else{
+      $('#tag-content').append(html)
+    }
+
+    return;
   },
-  publicMethods = {
-    addTag: function(tag, tagData){
-      var $self = $(this);
-      var opts = $self.data('opts');
-      var tlis = $self.data('tlis');
-      var html;
-
-      // Tag is empty then return empty
-      if(!tag){ return;}
-
-      // Is the tag restricted to a list?
-      if(opts.onlyTagList){
-        // List of available tags
-        var $tagList = opts.tagList;
-        // List of selected tags
-        var $tlis = tlis;
-
-        $.each(tlis, function(index, item){
-            $tlis[index] = item.toLowerCase();
-          });
-
-        $.each($tagList, function(index, item){
-          $tagList[index] = item.toLowerCase();
-        });
-
-        // If the tag is not on the list
-        if($.inArray(tag.toLowerCase(), $tagList) == -1){
-          return;
-        }
-
-        //If there is a validator function and the tag didn't make it
-        if(opts.validator && !opts.validator(tag)){
-          $self.trigger('tagInvalid',tag);
-          return;
-        }
-
-        // Do not let any more tags if it's over the limit
-        if(!opts.maxTags == -1 && opts.maxTags > tlis.length){
-          return ;
-        }
-
-        // There cannot be duplicates
-        if(!allowDuplicates && $.inArray(tag.toLowerCase(), $tlis) == -1){
-          $self.trigger('tagDuplicate',tag)
-          return;
-        }
-
-        html+= "<span class="+opts.tagClass+">"+tag+"<a href='#' id='tagger'>"+opts.tagCloseIcon+"</a></span>";
-
-        $(el) = $(html);
-
-        $(opts.tagsContainer).append($(el))
-
-        /*        
-          TO DO: Remove event binding
-        */
-      }
+  tagListChecker: function(tag){
+    var self = $(this)[0];
+    if($.inArray(tag, self.tagList) >= 0){
+      return true;
+    }else{
+      alert("Error!");
+      console.log("Not in list")
+      return false;
     }
   },
-  privateMethods = {
-    init: function(options){
-      var opts = $.extend({}, defaults, options);
+  noDuplicate: function(tag){
+    var tlis = $('#tag-content > .label');
+    if(tlis.length == 0){
+      return true;
+    }else{
+      $.each(tlis, function(index, item){
+        if(item == tag){
+          alert("Error!");
+          console.log("Duplication")
+          return false;
+        }
+      })
+      return true;
+    }
+  },
+  tagListener: function(){
+    var self = $(this)[0];
+    $('#'+self.inputId).on('focus keypress', function(){
+      console.log("Dem tags");
+    })
+  },
+  init: function(){
+    var self = $(this)[0];
+    var html = "";
+    
+    /*
+      Checking for mandatory flags
+    */
 
-      // Set validator stance
-      if (!$.isFunction(opts.validator)) { opts.validator = null; }
+    if(!self.inputId){ console.log("No input ID provided"); return;}
+    if(self.onlyTagList && self.tagList.length == 0 && self.tagListContainerId){
+      console.log("Tag list problem detected"); 
+      return;
+    }else{
+      // Populating tag list
+      $.each(self.tagList, function(index, item){
+        html+= "<li><a href='#'>"+item+"</a></li>"
+      })
 
-      this.each(function(){
-        var $self = $(this);
+      // Setting necessary flags
+      $('#'+self.tagListContainerId).append(html);
+      $('#'+self.inputId).attr('data-dropdown', self.tagListContainerId)
+        .attr('aria-controls', self.tagListContainerId)
+        .attr('aria-expanded', 'false')
 
-        // Data elements
-        if ($self.data('tagger')) { return false; }
-        $self.data('tagger', true);
+      // Recalling foundation to bind events
+      $(document).foundation('reflow');
 
-        $self.data('opts', opts);
+      // Click event for dropdown
+      $('#'+self.tagListContainerId +' > li > a').on('click', function(){
+        $('#'+self.inputId).val($(this).text());
+        self.addTag($('#'+self.inputId).val())
+      })
 
-        // Event handling
-        $.on('keypress', function(e){
-          console.log(e)
-        })
+      // Remove event for labels
+      $(document).on('click', '#tag-content > .label > a', function(){
+        $(this).parent().remove();
       })
     }
-  };
-
-  $.fn.tagger = function(method) {
-        var $self = $(this);
-
-        if (!(0 in this)) { return this; }
-
-        if ( publicMethods[method] ) {
-            return publicMethods[method].apply( $self, Array.prototype.slice.call(arguments, 1) );
-        } else if ( typeof method === 'object' || ! method ) {
-            return privateMethods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method ' +  method + ' does not exist.' );
-            return false;
-        }
-    };
-})();
+  }
+}
