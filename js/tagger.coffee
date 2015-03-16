25159df35c6bb08655734943c7d2ac5d40b7d964
Ramon Rodrigues <cerberus.ramon@gmail.com>
2015-03-16 14:42:35 +0000

    Refactored code, removed a function, added html file

--- js/tagger.coffee
+++ js/tagger.coffee
@@ -6,39 +6,132 @@ taggerJS =
     main.data 'tagger',
       options: options
     # TO DO: Check flags
-    self = main.data('tagger').options
-    alert 'Some flags are missing' unless self.hiddenInputId and self.buttonId and self.tagContainerId and self.tagListContainerId and self.tagListContainerHeight
-
-    if self.tagList
+    data = main.data('tagger').options
+    alert 'Some flags are missing' unless data.hiddenInputId and data.buttonId and data.tagContainerId and data.tagListContainerId and data.tagListContainerHeight
+    if data.tagList
       taggerJS.populateDropdown.apply main
       $(document).foundation('dropdown', 'reflow');
       taggerJS.tagListClickEvent.apply main
-    # Check if there is some tags already selected
-    if self.tagListStart
-      if self.tagListFormat
-        idPos = $.inArray 'id', self.tagListFormat
-        namePos = $.inArray 'name', self.tagListFormat
-        for tag in self.tagListStart
-          taggerJS.addTag.apply main, tag
-          self.indexableTagList.push tag[namePos]
-      else
-        for tag in self.tagListStart
-          taggerJS.addTag.apply main, tag
-          self.indexableTagList.push tag
 
-    taggerJS.setTagListeners
+    if data.tagListStart
+      if data.tagListFormat
+        idPos = $.inArray 'id', data.tagListFormat
+        namePos = $.inArray 'name', data.tagListFormat
+        for tag in data.tagListStart
+          taggerJS.addTag.apply main, tag
+          data.indexableTagList.push tag[namePos]
+      else
+        for tag in data.tagListStart
+          taggerJS.addTag.apply main, tag
+          data.indexableTagList.push tag
+
+    taggerJS.setTagListeners.apply main
   addTag: (item, value)->
-    console.log "#{item} #{value}"
+    main = $(this)
+    data = main.data('tagger').options
+    flag = true
+    html = "<span class='label #{data.labelClass}'>#{value} <a id='tagger-remove-label' data-value=#{item || value} href='#'>#{data.tagCloseIcon}</a></span>"
+
+    if !data.allowDuplicates and !data.onlyTagList
+      flag = taggerJS.noDuplicate.apply main, [value]
+    if !data.allowDuplicates and data.onlyTagList
+      flag = taggerJS.noDuplicate.apply(main, [value]) and taggerJS.isInTagList.apply(main, [value])
+    if data.allowDuplicates and taggerJS.onlyTagList
+      flag = taggerJS.isInTagList.apply main, [value]
+
+    $("##{data.tagContainerId}").append(html) if flag
+    if  $("##{data.hiddenInputId}").val()
+      $("##{data.hiddenInputId}").val($("##{data.hiddenInputId}").val()+",#{item || value}")
+    else
+      $("##{data.hiddenInputId}").val(item || value)
+
+  filterInput: (str) ->
+    main = $(this)
+    data = main.data('tagger').options
+    regexp = new RegExp(str, 'i')
+    $.each $("##{data.tagListContainerId}").find('ul').find('li'), (i, v) ->
+      ($(this).hide() unless regexp.test($(this).find('a').find('h6').text()))
+      return
+
+  isInTagList: (value) ->
+    main = $(this)
+    data = main.data('tagger').options
+    $.inArray(value, data.indexableTagList) >= 0
+
+  noDuplicate: (value)->
+    main = $(this)
+    data = main.data('tagger').options
+    arr = []
+    $.each $("##{data.tagContainerId} > span"), (i, val) ->
+      arr.push($(this).text().slice(0,-2))
+      return
+    $.inArray(value, arr) == -1
 
   populateDropdown: ->
+    main = $(this)
+    data = main.data('tagger').options
+    html = ''
+    if data.tagListFormat
+      idPos = $.inArray 'id', data.tagListFormat
+      namePos = $.inArray 'name', data.tagListFormat
+      $.each data.tagList, (index, item) ->
+        html += "<li data-value=#{item[idPos]}><a href='javascript:void(0)'><h6>#{item[namePos]}</h6></a></li>"
+        data.indexableTagList.push(item[namePos])
+        return
+    else
+      $.each data.tagList, (index, item) ->
+        html += "<li data-value=#{item}><a href='javascript:void(0)'><h6>#{item}</h6></a></li>"
+        data.indexableTagList.push(item)
+        return
+
+    html = "<div id=#{data.tagListContainerId} class='f-dropdown medium content' data-dropdown-content aria-autoclose='false' aria-hidden='true' tabindex='-1'><input id=#{data.filterId} type='text'><ul class='inline-list' style='height: #{data.tagListContainerHeight}px; overflow:auto;'>#{html}</ul></div>"
+
+    # Adding HTML to page and linking dropdown to button
+    $("##{data.hiddenInputId}").parent().append(html)
+    $("##{data.buttonId}").attr('data-dropdown', data.tagListContainerId)
+      .attr('aria-controls', data.tagListContainerId)
+      .attr('aria-expanded', 'false')
+
+  removeLabelFromHiddenInput: (value)->
+    main = $(this)
+    data = main.data('tagger').options
+    arr = $("##{data.hiddenInputId}").val().split(",")
+    i = arr.indexOf("#{value}")
+    arr.splice(i,1)
+    $("##{data.hiddenInputId}").val(arr.join())
+
+  setTagListeners: (self)->
+    main = $(this)
+    data = main.data('tagger').options
+    #Listener for the input filter, and creator of tags
+    $("##{data.filterId}").keyup (evt) ->
+      if evt.keyCode == 188 and !data.onlyTagList
+        taggerJS.addTag.apply main, [$(this).val().split(",")[0], $(this).val().split(",")[0]]
+        $(this).val('')
+      taggerJS.filterInput.apply main, [$(this).val()]
+      if $(this).val().length == 0
+        $("##{data.tagListContainerId} > ul > li").show()
+    # Click on the remove icon
+    $(document).on 'click', "[id='tagger-remove-label']", ->
+      taggerJS.toggleVisiblityTagList.apply main, [$(this).data('value')]
+      # 1 Remove from hidden input
+      taggerJS.removeLabelFromHiddenInput.apply main, [$(this).data('value')]
+      # 2 Remove label
+      $(this).parent().remove()
+
+  tagListClickEvent: ->
     # This function is called if there is a tag list
     main = $(this)
-    self = main.data('tagger').options
+    data = main.data('tagger').options
+    $("##{data.tagListContainerId} > ul > li").click (evt) ->
+      evt.preventDefault()
+      taggerJS.toggleVisiblityTagList.apply main, [$(this).data('value')]
+      taggerJS.addTag.apply main, [$(this).data('value'), $(this).find('a').find('h6').text()]
 
-  setTagListeners:  ->
-    console.log "TagListeners"
-  tagListClickEvent: ->
-    console.log "TagList"
+  toggleVisiblityTagList: (value)->
+    main = $(this)
+    data = main.data('tagger').options
+    $("##{data.tagListContainerId} > ul > li[data-value=#{value}]").toggle() unless data.allowDuplicates
 
   default_options:
     allowDuplicates: true
@@ -46,7 +139,7 @@ taggerJS =
     filterId: 'tagger-filter'
     hiddenInputId: null
     indexableTagList: []
-    labelClass: null
+    labelClass: ''
     onlyTagList: false
     tagCloseIcon: 'X'
     tagContainerId: null
--- js/tagger.js
+++ js/tagger.js
@@ -2,55 +2,161 @@ var taggerJS;
 
 taggerJS = {
   init: function(options) {
-    var idPos, main, namePos, self, tag, _i, _j, _len, _len1, _ref, _ref1;
+    var data, idPos, main, namePos, tag, _i, _j, _len, _len1, _ref, _ref1;
     options = $.extend({}, taggerJS.default_options, options);
     main = $(this);
     main.data('tagger', {
       options: options
     });
-    self = main.data('tagger').options;
-    if (!(self.hiddenInputId && self.buttonId && self.tagContainerId && self.tagListContainerId && self.tagListContainerHeight)) {
+    data = main.data('tagger').options;
+    if (!(data.hiddenInputId && data.buttonId && data.tagContainerId && data.tagListContainerId && data.tagListContainerHeight)) {
       alert('Some flags are missing');
     }
-    if (self.tagList) {
+    if (data.tagList) {
       taggerJS.populateDropdown.apply(main);
       $(document).foundation('dropdown', 'reflow');
       taggerJS.tagListClickEvent.apply(main);
     }
-    if (self.tagListStart) {
-      if (self.tagListFormat) {
-        idPos = $.inArray('id', self.tagListFormat);
-        namePos = $.inArray('name', self.tagListFormat);
-        _ref = self.tagListStart;
+    if (data.tagListStart) {
+      if (data.tagListFormat) {
+        idPos = $.inArray('id', data.tagListFormat);
+        namePos = $.inArray('name', data.tagListFormat);
+        _ref = data.tagListStart;
         for (_i = 0, _len = _ref.length; _i < _len; _i++) {
           tag = _ref[_i];
           taggerJS.addTag.apply(main, tag);
-          self.indexableTagList.push(tag[namePos]);
+          data.indexableTagList.push(tag[namePos]);
         }
       } else {
-        _ref1 = self.tagListStart;
+        _ref1 = data.tagListStart;
         for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
           tag = _ref1[_j];
           taggerJS.addTag.apply(main, tag);
-          self.indexableTagList.push(tag);
+          data.indexableTagList.push(tag);
         }
       }
     }
-    return taggerJS.setTagListeners;
+    return taggerJS.setTagListeners.apply(main);
   },
   addTag: function(item, value) {
-    return console.log("" + item + " " + value);
+    var data, flag, html, main;
+    main = $(this);
+    data = main.data('tagger').options;
+    flag = true;
+    html = "<span class='label " + data.labelClass + "'>" + value + " <a id='tagger-remove-label' data-value=" + (item || value) + " href='#'>" + data.tagCloseIcon + "</a></span>";
+    if (!data.allowDuplicates && !data.onlyTagList) {
+      flag = taggerJS.noDuplicate.apply(main, [value]);
+    }
+    if (!data.allowDuplicates && data.onlyTagList) {
+      flag = taggerJS.noDuplicate.apply(main, [value]) && taggerJS.isInTagList.apply(main, [value]);
+    }
+    if (data.allowDuplicates && taggerJS.onlyTagList) {
+      flag = taggerJS.isInTagList.apply(main, [value]);
+    }
+    if (flag) {
+      $("#" + data.tagContainerId).append(html);
+    }
+    if ($("#" + data.hiddenInputId).val()) {
+      return $("#" + data.hiddenInputId).val($("#" + data.hiddenInputId).val() + ("," + (item || value)));
+    } else {
+      return $("#" + data.hiddenInputId).val(item || value);
+    }
+  },
+  filterInput: function(str) {
+    var data, main, regexp;
+    main = $(this);
+    data = main.data('tagger').options;
+    regexp = new RegExp(str, 'i');
+    return $.each($("#" + data.tagListContainerId).find('ul').find('li'), function(i, v) {
+      if (!regexp.test($(this).find('a').find('h6').text())) {
+        $(this).hide();
+      }
+    });
+  },
+  isInTagList: function(value) {
+    var data, main;
+    main = $(this);
+    data = main.data('tagger').options;
+    return $.inArray(value, data.indexableTagList) >= 0;
+  },
+  noDuplicate: function(value) {
+    var arr, data, main;
+    main = $(this);
+    data = main.data('tagger').options;
+    arr = [];
+    $.each($("#" + data.tagContainerId + " > span"), function(i, val) {
+      arr.push($(this).text().slice(0, -2));
+    });
+    return $.inArray(value, arr) === -1;
   },
   populateDropdown: function() {
-    var main, self;
+    var data, html, idPos, main, namePos;
     main = $(this);
-    return self = main.data('tagger').options;
+    data = main.data('tagger').options;
+    html = '';
+    if (data.tagListFormat) {
+      idPos = $.inArray('id', data.tagListFormat);
+      namePos = $.inArray('name', data.tagListFormat);
+      $.each(data.tagList, function(index, item) {
+        html += "<li data-value=" + item[idPos] + "><a href='javascript:void(0)'><h6>" + item[namePos] + "</h6></a></li>";
+        data.indexableTagList.push(item[namePos]);
+      });
+    } else {
+      $.each(data.tagList, function(index, item) {
+        html += "<li data-value=" + item + "><a href='javascript:void(0)'><h6>" + item + "</h6></a></li>";
+        data.indexableTagList.push(item);
+      });
+    }
+    html = "<div id=" + data.tagListContainerId + " class='f-dropdown medium content' data-dropdown-content aria-autoclose='false' aria-hidden='true' tabindex='-1'><input id=" + data.filterId + " type='text'><ul class='inline-list' style='height: " + data.tagListContainerHeight + "px; overflow:auto;'>" + html + "</ul></div>";
+    $("#" + data.hiddenInputId).parent().append(html);
+    return $("#" + data.buttonId).attr('data-dropdown', data.tagListContainerId).attr('aria-controls', data.tagListContainerId).attr('aria-expanded', 'false');
   },
-  setTagListeners: function() {
-    return console.log("TagListeners");
+  removeLabelFromHiddenInput: function(value) {
+    var arr, data, i, main;
+    main = $(this);
+    data = main.data('tagger').options;
+    arr = $("#" + data.hiddenInputId).val().split(",");
+    i = arr.indexOf("" + value);
+    arr.splice(i, 1);
+    return $("#" + data.hiddenInputId).val(arr.join());
+  },
+  setTagListeners: function(self) {
+    var data, main;
+    main = $(this);
+    data = main.data('tagger').options;
+    $("#" + data.filterId).keyup(function(evt) {
+      if (evt.keyCode === 188 && !data.onlyTagList) {
+        taggerJS.addTag.apply(main, [$(this).val().split(",")[0], $(this).val().split(",")[0]]);
+        $(this).val('');
+      }
+      taggerJS.filterInput.apply(main, [$(this).val()]);
+      if ($(this).val().length === 0) {
+        return $("#" + data.tagListContainerId + " > ul > li").show();
+      }
+    });
+    return $(document).on('click', "[id='tagger-remove-label']", function() {
+      taggerJS.toggleVisiblityTagList.apply(main, [$(this).data('value')]);
+      taggerJS.removeLabelFromHiddenInput.apply(main, [$(this).data('value')]);
+      return $(this).parent().remove();
+    });
   },
   tagListClickEvent: function() {
-    return console.log("TagList");
+    var data, main;
+    main = $(this);
+    data = main.data('tagger').options;
+    return $("#" + data.tagListContainerId + " > ul > li").click(function(evt) {
+      evt.preventDefault();
+      taggerJS.toggleVisiblityTagList.apply(main, [$(this).data('value')]);
+      return taggerJS.addTag.apply(main, [$(this).data('value'), $(this).find('a').find('h6').text()]);
+    });
+  },
+  toggleVisiblityTagList: function(value) {
+    var data, main;
+    main = $(this);
+    data = main.data('tagger').options;
+    if (!data.allowDuplicates) {
+      return $("#" + data.tagListContainerId + " > ul > li[data-value=" + value + "]").toggle();
+    }
   },
   default_options: {
     allowDuplicates: true,
@@ -58,7 +164,7 @@ taggerJS = {
     filterId: 'tagger-filter',
     hiddenInputId: null,
     indexableTagList: [],
-    labelClass: null,
+    labelClass: '',
     onlyTagList: false,
     tagCloseIcon: 'X',
     tagContainerId: null,
